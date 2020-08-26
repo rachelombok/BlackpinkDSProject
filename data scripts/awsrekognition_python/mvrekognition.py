afbc0970c52611ecf3635b2b8d7a716054d2c5d9
Rachel Ombok <rbo224@nyu.edu>
2020-08-26 01:23:10 -0400

    restructure files and add AWS Rekognition

---
+++ data scripts/awsrekognition_python/mvrekognition.py
@@ -0,0 +1,238 @@
+#arn:aws:sns:us-east-1:378028176432:AmazonRekognitionbp
+
+#Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+import boto3
+import json
+import sys
+import time
+
+class VideoDetect:
+    jobId = ''
+    rek = boto3.client('rekognition')
+    sqs = boto3.client('sqs')
+    sns = boto3.client('sns')
+    
+    roleArn = ''
+    bucket = ''
+    video = ''
+    startJobId = ''
+
+    sqsQueueUrl = ''
+    snsTopicArn = ''
+    processType = ''
+
+    def __init__(self, role, bucket, video):    
+        self.roleArn = role
+        self.bucket = bucket
+        self.video = video
+
+    def GetSQSMessageSuccess(self):
+
+        jobFound = False
+        succeeded = False
+    
+        dotLine=0
+        while jobFound == False:
+            sqsResponse = self.sqs.receive_message(QueueUrl=self.sqsQueueUrl, MessageAttributeNames=['ALL'],
+                                          MaxNumberOfMessages=10)
+
+            if sqsResponse:
+                
+                if 'Messages' not in sqsResponse:
+                    if dotLine<40:
+                        print('.', end='')
+                        dotLine=dotLine+1
+                    else:
+                        print()
+                        dotLine=0    
+                    sys.stdout.flush()
+                    time.sleep(5)
+                    continue
+
+                for message in sqsResponse['Messages']:
+                    notification = json.loads(message['Body'])
+                    rekMessage = json.loads(notification['Message'])
+                    print(rekMessage['JobId'])
+                    print(rekMessage['Status'])
+                    if rekMessage['JobId'] == self.startJobId:
+                        print('Matching Job Found:' + rekMessage['JobId'])
+                        jobFound = True
+                        if (rekMessage['Status']=='SUCCEEDED'):
+                            succeeded=True
+
+                        self.sqs.delete_message(QueueUrl=self.sqsQueueUrl,
+                                       ReceiptHandle=message['ReceiptHandle'])
+                    else:
+                        print("Job didn't match:" +
+                              str(rekMessage['JobId']) + ' : ' + self.startJobId)
+                    # Delete the unknown message. Consider sending to dead letter queue
+                    self.sqs.delete_message(QueueUrl=self.sqsQueueUrl,
+                                   ReceiptHandle=message['ReceiptHandle'])
+
+
+        return succeeded
+
+    def StartLabelDetection(self):
+        response=self.rek.start_label_detection(Video={'S3Object': {'Bucket': self.bucket, 'Name': self.video}},
+            NotificationChannel={'RoleArn': self.roleArn, 'SNSTopicArn': self.snsTopicArn})
+
+        self.startJobId=response['JobId']
+        print('Start Job Id: ' + self.startJobId)
+
+
+    def GetLabelDetectionResults(self):
+        maxResults = 10
+        paginationToken = ''
+        finished = False
+        jsonlst = []
+        # store all label details in this list, to be turned into a json file
+
+        while finished == False:
+            response = self.rek.get_label_detection(JobId=self.startJobId,
+                                            MaxResults=maxResults,
+                                            NextToken=paginationToken,
+                                            SortBy='TIMESTAMP')
+
+            print('Codec: ' + response['VideoMetadata']['Codec'])
+            print('Duration: ' + str(response['VideoMetadata']['DurationMillis']))
+            print('Format: ' + response['VideoMetadata']['Format'])
+            print('Frame rate: ' + str(response['VideoMetadata']['FrameRate']))
+            print()
+
+            for labelDetection in response['Labels']:
+                newdct = {}
+                # a dictionary that will store all the data of the current label
+                label=labelDetection['Label']
+
+                print("Timestamp: " + str(labelDetection['Timestamp']))
+                print("   Label: " + label['Name'])
+                print("   Confidence: " +  str(label['Confidence']))
+                print("   Instances:")
+                if label['Confidence'] > 65:
+                    # only store data that has a confidence over 65
+                    newdct['timestamp'] = str(labelDetection['Timestamp'])
+                    newdct['label'] = label['Name']
+                    newdct['confidence'] = str(label['Confidence'])
+                    newdct['instances'] = 0
+                if 'Instances' in label:
+                    for instance in label['Instances']:
+                        print ("      Confidence: " + str(instance['Confidence']))
+                        print ("      Bounding box")
+                        print ("        Top: " + str(instance['BoundingBox']['Top']))
+                        print ("        Left: " + str(instance['BoundingBox']['Left']))
+                        print ("        Width: " +  str(instance['BoundingBox']['Width']))
+                        print ("        Height: " +  str(instance['BoundingBox']['Height']))
+                        print()
+                    if label['Confidence'] > 65:  
+                        # only store data that has a confidence over 65 
+                        newdct['instances'] = len(label['Instances'])
+                print()
+
+                if label['Confidence'] > 65:
+                    # only store data that has a confidence over 65
+                    newdct['parents'] = []
+                print ("   Parents:")
+                if 'Parents' in label:
+                    for parent in label['Parents']:
+                        print ("      " + parent['Name'])
+                        if label['Confidence'] > 65:
+                            # only store data that has a confidence over 65
+                            newdct['parents'].append(parent['Name'])
+                print ()
+
+                if label['Confidence'] > 65:
+                    # only add the dictionary if the confidence was over 65
+                    jsonlst.append(newdct)
+                
+
+                if 'NextToken' in response:
+                    paginationToken = response['NextToken']
+                else:
+                    finished = True
+
+        jsonobj = json.dumps(jsonlst, indent=2)
+        # dump the list data to a json file
+        with open("sample2.json", "w") as outfile: 
+            # create a new json file for all the labels
+            outfile.write(jsonobj)
+       
+    
+    def CreateTopicandQueue(self):
+      
+        millis = str(int(round(time.time() * 1000)))
+
+        #Create SNS topic
+        
+        snsTopicName="AmazonRekognitionExample" + millis
+
+        topicResponse=self.sns.create_topic(Name=snsTopicName)
+        self.snsTopicArn = topicResponse['TopicArn']
+
+        #create SQS queue
+        sqsQueueName="AmazonRekognitionQueue" + millis
+        self.sqs.create_queue(QueueName=sqsQueueName)
+        self.sqsQueueUrl = self.sqs.get_queue_url(QueueName=sqsQueueName)['QueueUrl']
+ 
+        attribs = self.sqs.get_queue_attributes(QueueUrl=self.sqsQueueUrl,
+                                                    AttributeNames=['QueueArn'])['Attributes']
+                                        
+        sqsQueueArn = attribs['QueueArn']
+
+        # Subscribe SQS queue to SNS topic
+        self.sns.subscribe(
+            TopicArn=self.snsTopicArn,
+            Protocol='sqs',
+            Endpoint=sqsQueueArn)
+
+        #Authorize SNS to write SQS queue 
+        policy = """{{
+  "Version":"2012-10-17",
+  "Statement":[
+    {{
+      "Sid":"MyPolicy",
+      "Effect":"Allow",
+      "Principal" : {{"AWS" : "*"}},
+      "Action":"SQS:SendMessage",
+      "Resource": "{}",
+      "Condition":{{
+        "ArnEquals":{{
+          "aws:SourceArn": "{}"
+        }}
+      }}
+    }}
+  ]
+}}""".format(sqsQueueArn, self.snsTopicArn)
+ 
+        response = self.sqs.set_queue_attributes(
+            QueueUrl = self.sqsQueueUrl,
+            Attributes = {
+                'Policy' : policy
+            })
+
+    def DeleteTopicandQueue(self):
+        self.sqs.delete_queue(QueueUrl=self.sqsQueueUrl)
+        self.sns.delete_topic(TopicArn=self.snsTopicArn)
+
+
+def main():
+    roleArn = 'arn:aws:iam::378028176432:role/bpdata'   
+    # roleArn is the IAM Service role ARN from AWS
+    bucket = 'blackpinkdata'
+    # name of the S3 bucket where the video is
+    video = 'boombayahmv.mp4'
+    # name of the video
+
+    analyzer=VideoDetect(roleArn, bucket,video)
+    analyzer.CreateTopicandQueue()
+
+    analyzer.StartLabelDetection()
+    if analyzer.GetSQSMessageSuccess()==True:
+        analyzer.GetLabelDetectionResults()
+    
+    analyzer.DeleteTopicandQueue()
+
+
+if __name__ == "__main__":
+    main()
