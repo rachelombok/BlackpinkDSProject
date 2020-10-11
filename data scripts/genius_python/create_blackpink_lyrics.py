e62616a71e14ca29cf658a2ed3b5c8fb4bc5b8bc
Rachel Ombok <rbo224@nyu.edu>
2020-10-11 14:22:34 -0400

    all lyrics

--- data scripts/genius_python/create_blackpink_lyrics.py
+++ data scripts/genius_python/create_blackpink_lyrics.py
@@ -13,7 +13,9 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) #spo
 
 kamu_uri = 'spotify:track:7jr3iPu4O4bTCVwLMbdU2i'
 
-song_titles = ['ddu-du-ddu-du', 'dont-know-what-to-do', 'kill-this-love', 'boombayah', 'whistle', 'playing-with-fire', 'stay', 'forever-young', 'see-u-later', 'really', 'kick-it', 'hope-not', 'as-if-its-your-last','how-you-like-that']
+song_titles = ['ddu-du-ddu-du', 'dont-know-what-to-do', 'kill-this-love', 'boombayah', 'whistle', 
+			'playing-with-fire', 'stay', 'forever-young', 'see-u-later', 'really', 'kick-it', 
+			'hope-not', 'as-if-its-your-last','how-you-like-that','lovesick-girls','pretty-savage','you-never-know']
 
 res = []
 for i in range(len(song_titles)):
@@ -26,16 +28,28 @@ for i in range(len(song_titles)):
 kamu = requests.get('https://genius.com/Dua-lipa-and-blackpink-kiss-and-make-up-lyrics')
 srcndy = requests.get('https://genius.com/Genius-english-translations-lady-gaga-and-blackpink-sour-candy-english-translation-lyrics')
 icecream = requests.get('https://genius.com/Genius-english-translations-blackpink-and-selena-gomez-ice-cream-english-translation-lyrics')
+cfy = requests.get('https://genius.com/Blackpink-crazy-over-you-lyrics')
+byw = requests.get('https://genius.com/Blackpink-bet-you-wanna-lyrics')
+lthm = requests.get('https://genius.com/Blackpink-love-to-hate-me-lyrics')
 soup_one = BeautifulSoup(kamu.text, 'lxml')
 soup_two = BeautifulSoup(srcndy.text, 'lxml')
 soup_three = BeautifulSoup(icecream.text, 'lxml')
+soup_four = BeautifulSoup(cfy.text, 'lxml')
+soup_five = BeautifulSoup(byw.text, 'lxml')
+soup_six = BeautifulSoup(lthm.text, 'lxml')
 lyrics_one = soup_one.find('div', class_='lyrics').get_text()
 lyrics_two = soup_two.find('div', class_='lyrics').get_text()
 lyrics_three = soup_three.find('div', class_='lyrics').get_text()
+lyrics_four = soup_four.find('div', class_='lyrics').get_text()
+lyrics_five = soup_five.find('div', class_='lyrics').get_text()
+lyrics_six = soup_six.find('div', class_='lyrics').get_text()
 
 res.append(['sour-candy', lyrics_two])
 res.append(['ice-cream', lyrics_three])
 res.append(['kiss-and-make-up', lyrics_one])
+res.append(['crazy-for-you', lyrics_four])
+res.append(['bet-you-wanna', lyrics_five])
+res.append(['love-to-hate-me', lyrics_six])
 
 index = []
 lyrics = {}
